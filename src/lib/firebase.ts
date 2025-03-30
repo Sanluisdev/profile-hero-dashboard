569937c1fd67119b5ebf4bceef1c9f3c994af65e
Sanluisdev <artesanluis00@gmail.com>
2025-03-30 18:24:41 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,8 +8,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebas
 // Normalmente esto debería ser un secreto, pero Firebase permite que estas claves sean públicas
 // ya que se restringen con reglas de seguridad en el backend
   const firebaseConfig = {
-    apiKey: "AIzaSyBK84VweShaBsOf4KgF30Iy5Z1GaCFQ3Bg",
-    authDomain: "crm-med-8b83e.firebaseapp.com",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: "crm-med-8b83e",
     storageBucket: "crm-med-8b83e.firebasestorage.app",
     messagingSenderId: "963483880309",
